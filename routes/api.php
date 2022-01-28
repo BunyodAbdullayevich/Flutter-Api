9ee9dc306c99547d5ce0c3e6fc13c9556e81e1c6
BunyodAbdullayevich <B.Abdullayevich.03@gmail.com>
2022-01-28 15:49:04 +0500

    Auth Api Done

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\TransactionController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +27,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::get('/categories', [CategoryController::class, 'index']);
 // Route::get('/categories/{category}', [CategoryController::class, 'show']);
 
-Route::apiResource('/categories', CategoryController::class);
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::apiResource('/categories', CategoryController::class);
+    
+    Route::apiResource('/transactions', TransactionController::class);
+});
 
-Route::apiResource('/transactions', TransactionController::class);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
